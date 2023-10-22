ff63a304b221d5ad9137d098063738399d8330fb
kurraNikhil <kurranikhil900@gmail.com>
2023-10-21 17:56:11 -0700

    file_copying.py

---
+++ python- File_Handling/file_copying.py
@@ -0,0 +1,3 @@
+# Copy the contents of one file to another
+with open("source.txt", "r") as source_file, open("destination.txt", "w") as destination_file:
+    destination_file.write(source_file.read())
